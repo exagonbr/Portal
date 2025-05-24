51a79aaa0f3b6be2904259bb6fa0b0b131ccaa10
Estevão Maia <estevao@programmer.net>
2025-05-24 09:48:48 -0300

    Menu12312323123

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: false,
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
@@ -115,7 +115,7 @@ const finalConfig = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
+    disable: false,
   }
 });
 
